99cadd0034d5d2493f190b0bb19363a300db46dd
unknown <biggie9925@hotmail.com>
2023-11-10 16:19:56 +1300

    course 100% completed upload

--- testing-frontend-mocks/util/http.test.js
+++ testing-frontend-mocks/util/http.test.js
@@ -1,5 +1,6 @@
 import { it, expect, vi } from 'vitest'
 import { sendDataRequest } from './http'
+import { HttpError } from './errors';
 
 //creating a dummy response data
 const testResponseData = {testKey: 'testData'}
@@ -13,6 +14,12 @@ const testFetch = vi.fn((url, options) => {
 
     //fetch returns a Promise
     return new Promise((resolve, reject) => {
+
+        //checking to ensure the data was transformed to string/json
+        if (typeof options.body !== 'string') {
+            return reject('Not a string')
+        }
+
         //fetch returns a status code and json data
         const testResponse = {
             //response.ok
@@ -35,5 +42,47 @@ it('should return any available response data', () => {
 
     const testData = {key: 'test'};
     return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+
+})
+
+it('should convert the provided data to json before sending the request', async () => {
+
+    const testData = {key: 'test'};
+    let errorMessage;
+
+    try {
+        await sendDataRequest(testData);
+    } catch (error) {
+        errorMessage = error;
+    }
+
+    expect(errorMessage).not.toBe('Not a string')
+
+})
+
+it('should throw an HttpError in case of non-ok responses', () => {
+
+    testFetch.mockImplementationOnce((url, options) => {
+
+        //fetch returns a Promise
+        return new Promise((resolve, reject) => {
+    
+            //fetch returns a status code and json data
+            const testResponse = {
+                //response.ok
+                ok: false,
+                //json function also returns a Promise why we have a nested Promise
+                json() {
+                    return new Promise((resolve, reject) => {
+                        resolve(testResponseData);
+                    })
+                }
+            }
+            resolve(testResponse);
+        })
+    });
+
+    const testData = {key: 'test'};
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 
 })
--- testing-side-effects/src/util/io.test.js
+++ testing-side-effects/src/util/io.test.js
@@ -2,6 +2,16 @@ import { it, expect, vi } from 'vitest';
 import writeData from './io';
 import { promises as fs } from 'fs';
 
+
+//previous test before mock introduced -> it actually calls 
+//the writeData function and creates a test.txt file which 
+//passes the test but may affect the production code
+//e.g. if we are testing a database call we dont actually want to write to the DB
+
+//with mock it creates a spy function for ALL built in 
+//functions in fs, or other internal or external modules
+//so we can make sure the function is called without actually calling it
+
 vi.mock('fs');
 vi.mock('path', () => {
     return {
@@ -13,50 +23,32 @@ vi.mock('path', () => {
     }
 });
 
+
 it('should execute the writeFile method', () => {
 
     const testData = 'Test';
     const testFilename = 'test.txt';
 
-    //previous test before mock introduced -> it actually calls 
-    //the writeData function and creates a test.txt file which 
-    //passes the test but may affect the production code
-    //e.g. if we are testing a database call we dont actually want to write to the DB
-    //return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-
-
-    //with mock it creates a spy function for ALL built in 
-    //functions in fs, or other internal or external modules
-    //so we can make sure the function is called without actually calling it
-
-
     //here we are calling our own function (writeData)
     writeData(testData, testFilename)
 
-
     //and inside writeData is a built in function fs.writeFile
     //so here we are checking that this function was called
-
-    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-    //expect(fs.writeFile).toHaveBeenCalledWith(testFilename, testData);
+    expect(fs.writeFile).toHaveBeenCalledWith(testFilename, testData);
 
 })
 
-
-it('should execute the writeData method', () => {
-
-    const testData = 'Test';
-    const testFilename = 'test.txt';
-    writeData(testData, testFilename)
-    expect(writeData(testData, testFilename)).toBeCalled();
-
-})
 
 it('should return a promise that resolves to no value if called', () => {
 
     const testData = 'Test';
     const testFilename = 'test.txt';
+
     writeData(testData, testFilename)
+
+    //here we are expecting that the writeData returns a resolved Promise
+    //the Promise is undefined because it is just writing to a file not returning data
     return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
 
 })
